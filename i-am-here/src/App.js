3367ec4987a5a3f5db05d0241c3b7a4af9c9ce45
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2020-06-03 19:19:51 -0300

    Adicionado login

--- i-am-here/src/App.js
+++ i-am-here/src/App.js
@@ -1,13 +1,23 @@
 
 import React from 'react';
 import Dashboard from './themes/Dashboard'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './Login'
 
 export default function App() {
   return (
     <>
       <Router>
-        <Dashboard></Dashboard>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard></Dashboard>
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
