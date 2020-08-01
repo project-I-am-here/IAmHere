3b0e3392f0f170251d7adee0623dd4262007e776
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2020-08-01 19:16:02 -0300

    Configurado pobnto e virgula e aspas no arquivo

--- Front-Vue/source/src/router/index.js
+++ Front-Vue/source/src/router/index.js
@@ -1,228 +1,228 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue'
+import Router from 'vue-router'
 
 // Dashboard Components
-import dashboard from "../views/dashboard";
+import dashboard from '../views/dashboard'
 
 // Users
-import users from "../views/User/users";
-import usersForms from "../views/User/usersForms";
+import users from '../views/User/users'
+import usersForms from '../views/User/usersForms'
 
 // Widgets
-import widgets from "../views/widgets";
+import widgets from '../views/widgets'
 
 // UI Components
-import alerts from "../views/ui-components/alerts";
-import badges from "../views/ui-components/badges";
-import breadcrumbs from "../views/ui-components/breadcrumbs";
-import buttons from "../views/ui-components/buttons";
-import carousel from "../views/ui-components/carousel";
-import dropdowns from "../views/ui-components/dropdowns";
-import icons from "../views/ui-components/icons";
-import modals from "../views/ui-components/modals";
-import paginations from "../views/ui-components/paginations";
-import progress from "../views/ui-components/progress";
-import tables from "../views/ui-components/tables";
-import typography from "../views/ui-components/typography";
-import tabs from "../views/ui-components/tabs";
-import tooltips from "../views/ui-components/tooltips";
+import alerts from '../views/ui-components/alerts'
+import badges from '../views/ui-components/badges'
+import breadcrumbs from '../views/ui-components/breadcrumbs'
+import buttons from '../views/ui-components/buttons'
+import carousel from '../views/ui-components/carousel'
+import dropdowns from '../views/ui-components/dropdowns'
+import icons from '../views/ui-components/icons'
+import modals from '../views/ui-components/modals'
+import paginations from '../views/ui-components/paginations'
+import progress from '../views/ui-components/progress'
+import tables from '../views/ui-components/tables'
+import typography from '../views/ui-components/typography'
+import tabs from '../views/ui-components/tabs'
+import tooltips from '../views/ui-components/tooltips'
 
 // Sample Pages
-import error404 from "../views/sample-pages/error-404";
-import error500 from "../views/sample-pages/error-500";
+import error404 from '../views/sample-pages/error-404'
+import error500 from '../views/sample-pages/error-500'
 
-import login from "../views/sample-pages/login";
-import register from "../views/sample-pages/register";
+import login from '../views/sample-pages/login'
+import register from '../views/sample-pages/register'
 
-import patient from "../views/Patient/patient";
-import clinic from "../views/Clinic/clinic";
-import professional from "../views/professional/professional";
-import schedule from "../views/schedule/schedule";
+import patient from '../views/Patient/patient'
+import clinic from '../views/Clinic/clinic'
+import professional from '../views/professional/professional'
+import schedule from '../views/schedule/schedule'
 
 // Sample Pages
-import profile from "../views/User/profile";
+import profile from '../views/User/profile'
 
-Vue.use(Router);
+Vue.use(Router)
 
-const isAuthenticated = !!localStorage.getItem("@soull-token");
+const isAuthenticated = !!localStorage.getItem('@soull-token')
 
 export default new Router({
-  linkActiveClass: "active",
+  linkActiveClass: 'active',
   routes: [
     {
-      path: "/widgets",
-      name: "widgets",
+      path: '/widgets',
+      name: 'widgets',
       component: widgets
     },
     {
-      path: "/404",
-      name: "error-404",
+      path: '/404',
+      name: 'error-404',
       component: error404
     },
     {
-      path: "/500",
-      name: "error-500",
+      path: '/500',
+      name: 'error-500',
       component: error500
     },
     {
-      path: "/register",
-      name: "register",
+      path: '/register',
+      name: 'register',
       component: register
     },
     {
-      path: "/alerts",
-      name: "alerts",
+      path: '/alerts',
+      name: 'alerts',
       component: alerts
     },
     {
-      path: "/badges",
-      name: "badges",
+      path: '/badges',
+      name: 'badges',
       component: badges
     },
     {
-      path: "/breadcrumbs",
-      name: "breadcrumbs",
+      path: '/breadcrumbs',
+      name: 'breadcrumbs',
       component: breadcrumbs
     },
     {
-      path: "/buttons",
-      name: "buttons",
+      path: '/buttons',
+      name: 'buttons',
       component: buttons
     },
     {
-      path: "/carousel",
-      name: "carousel",
+      path: '/carousel',
+      name: 'carousel',
       component: carousel
     },
     {
-      path: "/dropdowns",
-      name: "dropdowns",
+      path: '/dropdowns',
+      name: 'dropdowns',
       component: dropdowns
     },
     {
-      path: "/icons",
-      name: "icons",
+      path: '/icons',
+      name: 'icons',
       component: icons
     },
     {
-      path: "/modals",
-      name: "modals",
+      path: '/modals',
+      name: 'modals',
       component: modals
     },
     {
-      path: "/paginations",
-      name: "paginations",
+      path: '/paginations',
+      name: 'paginations',
       component: paginations
     },
     {
-      path: "/progress",
-      name: "progress",
+      path: '/progress',
+      name: 'progress',
       component: progress
     },
     {
-      path: "/tables",
-      name: "tables",
+      path: '/tables',
+      name: 'tables',
       component: tables
     },
     {
-      path: "/typography",
-      name: "typography",
+      path: '/typography',
+      name: 'typography',
       component: typography
     },
     {
-      path: "/tabs",
-      name: "tabs",
+      path: '/tabs',
+      name: 'tabs',
       component: tabs
     },
     {
-      path: "/tooltips",
-      name: "tooltips",
+      path: '/tooltips',
+      name: 'tooltips',
       component: tooltips
     },
     {
-      path: "/dashboard",
-      name: "dashboard",
+      path: '/dashboard',
+      name: 'dashboard',
       component: dashboard,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/users",
-      name: "users",
+      path: '/users',
+      name: 'users',
       component: users,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/profile",
-      name: "profile",
+      path: '/profile',
+      name: 'profile',
       component: profile,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/userForm",
-      name: "userForm",
+      path: '/userForm',
+      name: 'userForm',
       component: usersForms,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/patient",
-      name: "patient",
+      path: '/patient',
+      name: 'patient',
       component: patient,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/professional",
-      name: "professional",
+      path: '/professional',
+      name: 'professional',
       component: professional,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/clinic",
-      name: "clinic",
+      path: '/clinic',
+      name: 'clinic',
       component: clinic,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/schedule",
-      name: "schedule",
+      path: '/schedule',
+      name: 'schedule',
       component: schedule,
       beforeEnter: (to, from, next) => {
-        if (isAuthenticated) next();
-        else next("/login");
+        if (isAuthenticated) next()
+        else next('/login')
       }
     },
     {
-      path: "/login",
-      name: "login",
+      path: '/login',
+      name: 'login',
       component: login,
       beforeEnter: (to, from, next) => {
-        if (!isAuthenticated) next();
-        else next("/dashboard");
+        if (!isAuthenticated) next()
+        else next('/dashboard')
       },
       beforeRouteLeave: (to, from, next) => {
-        if (!isAuthenticated) next();
-        else next("/dashboard");
+        if (!isAuthenticated) next()
+        else next('/dashboard')
       }
     }
   ]
-});
+})
